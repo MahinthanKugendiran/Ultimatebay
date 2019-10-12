9932673ee62e173f20ff6a6ff7985220b566f5c1
mahinthan <mahinthan0007@gmail.com>
2019-10-12 23:00:48 +0530

    Add project files.

---
+++ Ultimatebay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ultimatebay.Startup))]
+namespace Ultimatebay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
